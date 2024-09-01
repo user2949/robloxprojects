f0263b9e9fc48cd1bfd9a4a869ea491e023152f5
Darryl Alcander Lim <103663597+user2949@users.noreply.github.com>
2024-09-02 05:46:13 +0700

    Create DualScript.lua

---
+++ scripts/DualScript.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Prison%20Life/Script.lua"))()
